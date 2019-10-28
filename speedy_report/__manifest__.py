2b0fff9ac495ce335c5661036b4f885b645f303d
Macbook AIR <macbookair@MacBook-Air-de-Macbook-1071.local>
2019-10-28 10:07:48 +0000

    [UPDATE] template speedy

---
+++ speedy_report/__manifest__.py
@@ -0,0 +1,32 @@
+{
+    'name': 'Template Speedy',
+
+    'description': """
+               Speedy Template
+            """,
+
+    'author': 'OPTESIS SA BY ANG',
+    'website': "http://www.Optesis.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'version': '1.2.0',
+    'category': 'Test',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'hr'],
+
+    'data': [
+        'views/custom_format.xml',
+        'views/speedy_external_view.xml',
+        'views/template_speedy_internal_report_purchase.xml',
+        'views/template_speedy_internal_report_stock.xml',
+        'views/template_speedy_internal_report_sale.xml',
+        'views/template_speedy_internal_report_account.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+
+}
